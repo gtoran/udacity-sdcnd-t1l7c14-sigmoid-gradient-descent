156a8cfed01f429f167db13d58a2ddbf2c84dab5
gtoran <git@gtoran.com>
2017-03-13 22:26:45 +0100

    sgd_update implementation, iterating over trainables and setting t.value

--- miniflow.py
+++ miniflow.py
@@ -258,4 +258,6 @@ def sgd_update(trainables, learning_rate=1e-2):
     # Example:
     # for t in trainables:
     #   t.value = your implementation here
-    pass
+
+    for t in trainables:
+        t.value -= learning_rate * t.gradients[t]
